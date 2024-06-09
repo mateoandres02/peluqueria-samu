88a49e0c54895591bae653eb3166285d8f983836
Mateo Andres <mateooandress02@gmail.com>
2024-06-09 12:06:49 -0300

    Implementacion de modal y sus estilos y funcion de click en la seleccion de fecha, y se agrega la guia de estilos

--- src/components/calendarRender.js
+++ src/components/calendarRender.js
@@ -1,6 +1,8 @@
 import esLocale from "@fullcalendar/core/locales/es";
+import { createModal } from "./modal.js";
 
 const d = document;
+createModal()
 
 export default function calendarRender () {
   let calendarEl = d.getElementById("calendar");
@@ -15,9 +17,29 @@ export default function calendarRender () {
         }
       }
     },
-    // dateClick: function() {
-    //   alert('un dia ha sido clickeado!')
-    // },
+    dateClick: function(info) {
+      const modal = d.getElementById("dateClickModal");
+      const eventDate = d.getElementById("eventDate");
+
+      // Set the date input value to the clicked date
+      eventDate.value = info.dateStr;
+
+      // Muestra el modal
+      modal.style.display = "block";
+
+      // Cerrar el modal por boton cerrar
+      const closeModal = d.getElementById("closeModal");
+      closeModal.onclick = function() {
+        modal.style.display = "none";
+      };
+
+      // Cerrar la modal cuando se clickea fuera de ella
+      window.onclick = function(e) {
+        if (e.target == modal) {
+          modal.style.display = "none";
+        }
+      };
+    },
     headerToolbar: { 
       left: 'dayGridMonth,timeGridWeek,timeGridDay myCustomButton',
       center: 'title',
---
+++ style-guide.md
@@ -0,0 +1,14 @@
+# Guia de estilos para proyecto Peluqueria Samuel
+
+## Colores primarios (blancos y negros)
+
+- dark-grey: #39383E
+- super-light-blue: #F8F4FC
+- white: #FFFFFF
+- light-grey(bg-form): #D9D9D9
+- light-gret(letter): #DFDEE3 
+- dark-blue: #383F59
+- line-grey: #4E4E56
+
+## Fuente a utilizar
+-Arial

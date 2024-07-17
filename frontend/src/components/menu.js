ebe9f14fa65138d04a2ff31b5a44db49245fca0f
Mateo Andres <mateooandress02@gmail.com>
2024-07-17 12:41:20 -0300

    cambios en la estilizacion y estructuracion de panel nav

--- frontend/src/components/menu.js
+++ frontend/src/components/menu.js
@@ -1,17 +1,22 @@
 import '/src/styles/menu.css';
 
 const menu = `
-    <header class="header">
-        <a href="/index.html" class="logo">
-            <p>Peluquería Samu</p>
-        </a>
-        <nav class="nav">
-            <a href="#">Calendario</a>
-            <a href="#">Funcionalidad 2</a>
-            <a href="#">Funcionalidad 3</a>
-            <a href="#">Funcionalidad 4</a>
+    <aside class="sidebar">
+        <div class="profile">
+            <img src="profile-icon.png" alt="Profile Icon" class="profile-icon">
+            <span class="profile-name">AdminSamu</span>
+        </div>
+        <nav class="menu">
+            <ul>
+                <li><a href="#seguimiento-caja"><i class="icon"></i>Seguimiento de caja</a></li>
+                <li><a href="#compartir-calendario"><i class="icon"></i>Compartir calendaior</a></li>
+                <li><a href="#administrar-calendario"><i class="icon"></i>Administrar Calendario</a></li>
+                <li><a href="#administrar-barberos"><i class="icon"></i>Administrar Barberos</a></li>
+                <li><a href="#configuracion"><i class="icon"></i>Generar Tabla Turnos</a></li>
+                <li><a href="#cerrar-sesion"><i class="icon"></i>Cerrar Sesion</a></li>
+            </ul>
         </nav>
-    </header>
+    </aside>
 `;
 
 export default menu;

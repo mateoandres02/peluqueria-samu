eaff361ae62229fa16f879837ac765ada8ba19b8
Mateo Andres <mateooandress02@gmail.com>
2024-07-17 13:37:53 -0300

    estilizacion de sidebar

--- frontend/src/components/menu.js
+++ frontend/src/components/menu.js
@@ -3,17 +3,17 @@ import '/src/styles/menu.css';
 const menu = `
     <aside class="sidebar">
         <div class="profile">
-            <img src="profile-icon.png" alt="Profile Icon" class="profile-icon">
+            <img src="../../public/assets/icons/profile.svg" alt="Profile Icon" class="profile-icon">
             <span class="profile-name">AdminSamu</span>
         </div>
         <nav class="menu">
             <ul>
-                <li><a href="#seguimiento-caja"><i class="icon"></i>Seguimiento de caja</a></li>
-                <li><a href="#compartir-calendario"><i class="icon"></i>Compartir calendaior</a></li>
-                <li><a href="#administrar-calendario"><i class="icon"></i>Administrar Calendario</a></li>
-                <li><a href="#administrar-barberos"><i class="icon"></i>Administrar Barberos</a></li>
-                <li><a href="#configuracion"><i class="icon"></i>Generar Tabla Turnos</a></li>
-                <li><a href="#cerrar-sesion"><i class="icon"></i>Cerrar Sesion</a></li>
+                <li><a href="#seguimiento-caja"><img src="../../public/assets/icons/cash-register.svg" alt="Ver caja" class="icon">Seguimiento de caja</a></li>
+                <li><a href="#compartir-calendario"><img src="../../public/assets/icons/share.svg" alt="Compartir calendario" class="icon">Compartir calendaior</a></li>
+                <li><a href="#administrar-calendario"><img src="../../public/assets/icons/calendar.svg" alt="Administrar calendario" class="icon">Administrar Calendario</a></li>
+                <li><a href="#administrar-barberos"><img src="../../public/assets/icons/edit-user.svg" alt="Administrar barberos" class="icon">Administrar Barberos</a></li>
+                <li><a href="#generar-tabla"><img src="../../public/assets/icons/table.svg" alt="Generar tabla" class="icon">Generar Tabla Turnos</a></li>
+                <li><a href="#cerrar-sesion"><img src="../../public/assets/icons/log-out.svg" alt="Cerrar sesion" class="icon">Cerrar Sesion</a></li>
             </ul>
         </nav>
     </aside>

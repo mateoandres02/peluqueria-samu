582b2aad2016a77023558c77b14d6787d78e1bb2
Mateo Andres <mateooandress02@gmail.com>
2024-07-20 14:27:01 -0300

    solucionando problemas login

--- frontend/public/login.js
+++ frontend/public/login.js
@@ -6,22 +6,22 @@ const loginForm = $('login-form')
 
 loginForm.addEventListener("submit", async (e)=>{
   e.preventDefault();
-  const username = $('user').value;
-  const password = $('password').value;
-
-  await fetch('/login', {
+  const Nombre = $('user').value;
+  const Contrasena = $('password').value;
+  console.log(typeof username)
+  await fetch('http://localhost:3001/login', {
     method:"POST",
     headers: {
       "Content-Type":"application/json",
     },
-    body: JSON.stringify({username, password})
+    body: JSON.stringify({Nombre, Contrasena})
   })
   .then(res => {
     if (res.ok) {
-      window.location.href = '/turns'
+      window.location.href = '/'
       console.log("sesion iniciada correctamente, entrando...")
     } else {
       return mensajeError.classList.toggle("escondido", false)
     }
-  })
+  }).catch(error=>{console.error(error)})
 })

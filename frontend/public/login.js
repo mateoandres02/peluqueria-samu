381f9df5aaf940e73742cab30731183498e8a1a0
facuandrean <facundoandrean22@gmail.com>
2024-07-22 18:41:06 -0300

    Login

--- frontend/public/login.js
+++ frontend/public/login.js
@@ -6,22 +6,22 @@ const loginForm = $('login-form')
 
 loginForm.addEventListener("submit", async (e)=>{
   e.preventDefault();
-  const username = $('user').value;
-  const password = $('password').value;
+  const Nombre = $('user').value;
+  const Contrasena = $('password').value;
 
-  await fetch('/login', {
+  await fetch('http://localhost:3001/login', {
     method:"POST",
     headers: {
       "Content-Type":"application/json",
     },
-    body: JSON.stringify({username, password})
+    body: JSON.stringify({Nombre, Contrasena})
   })
   .then(res => {
     if (res.ok) {
-      window.location.href = '/turns'
+      window.location.href = '/index.html'
       console.log("sesion iniciada correctamente, entrando...")
     } else {
       return mensajeError.classList.toggle("escondido", false)
     }
-  })
+  });
 })

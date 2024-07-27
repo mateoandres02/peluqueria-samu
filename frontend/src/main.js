23632aeec3296e85462243781bf9405728cfd6d4
facuandrean <facundoandrean22@gmail.com>
2024-07-27 00:19:37 -0300

    Solucionando problema del dateClick sin auth del user

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,32 +1,4 @@
-import renderIndexAdmin from "./components/indexAdminView.js";
-import renderIndexEmployee from "./components/indexEmployeeView.js";
-
-async function checkAuthentication() {
-
-    try {
-        
-        const response = await fetch('http://localhost:3001/verify-token', { credentials: 'include' });
-        
-        if (!response.ok) {
-            window.location.href = '/login.html';
-        } else {
-
-            // Leemos el cuerpo de la respuesta, en el cual recibimos el usuario logueado.
-            const data = await response.json(); 
-
-            if (data.user.Rol === "Empleado") {
-                renderIndexEmployee(data.user.Nombre);
-            } else if (data.user.Rol === "Admin") {
-                renderIndexAdmin(data.user.Nombre);
-            }
-            
-        } 
-
-    } catch (error) {
-        window.location.href = '/login.html';
-    };
-
-};
+import checkAuthentication from "./components/auth.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     checkAuthentication();

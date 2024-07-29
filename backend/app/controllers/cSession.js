d40815ee7a660a83355f794575eaf65a41c4196d
facuandrean <facundoandrean22@gmail.com>
2024-07-28 23:26:47 -0300

    Session finalizada

--- backend/app/controllers/cSession.js
+++ backend/app/controllers/cSession.js
@@ -17,28 +17,28 @@ const login = async (req, res) => {
         // Logueamos el usuario y lo validamos.
         const user = await UserRepository.login({ Nombre, Contrasena, Rol });
 
-        // Generamos el token.
+        // Generamos el token de acceso.
         const token = jwt.sign(
             {
                 Nombre: user.Nombre,
-                Contrasena: user.Contrasena,
                 Rol: user.Rol
             }, 
             config.secretJwtKey, 
             {
-                expiresIn: '20m',
+                expiresIn: '10h',
             }
         );
 
-        // Guardamos el token en una cookie y enviamos la información del usuario logueado.
+        // Guardamos el token de acceso en una cookie.
         res.cookie('access_token', token, {
             httpOnly: true, // Asegura que la cookie no sea accesible desde JavaScript en el navegador
             // secure: true, // Asegura que la cookie solo se envíe a través de HTTPS
             // secure: process.env.NODE_ENV === 'production', // Asegura que la cookie se pueda probar en producción.
             sameSite: 'strict', // Previene que la cookie sea enviada en solicitudes cross-site
-            maxAge: 20 * 60 * 1000 // 20 minutos
+            maxAge: 10 * 60 * 60 * 1000 // 10 horas.
         });
         
+        // enviamos la información del usuario logueado y la información de los tokens
         res.send({ user, token });
 
     } catch (error) {

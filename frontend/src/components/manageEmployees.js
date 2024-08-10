bf5508fc92574d7a22ba027923852f866e8f3cc8
facuandrean <facundoandrean22@gmail.com>
2024-08-10 20:50:43 -0300

    Modificando estilos v2

--- frontend/src/components/manageEmployees.js
+++ frontend/src/components/manageEmployees.js
@@ -4,7 +4,7 @@ let manageEmployeesView = '<div class="manageEmployeesContainer"></div>';
 
 const postEmployee = `
   <div class="postEmployee">
-    <h3 class="postEmployee-h3">Administración de Empleados</h3>
+    <h3>Administración de Empleados</h3>
     <p class="postEmployee-p">Puede agregar nuevos empleados o quitarlos, además de cambiar su nombre y contraseña.</p>
     <button type="button" class="postEmployee-btn">
       <img src="../../public/assets/icons/person-fill-add.svg">
@@ -26,7 +26,7 @@ const modal = `
         <div class="modal-body">
 
           <form id="formPOSTEmployee">
-            <label for="username">Nombre</label>
+            <label for="username">Usuario</label>
             <input type="text" id="username" name="Nombre" class="input" required>
 
             <label for="password">Constraseña</label>
@@ -98,7 +98,7 @@ const usersData = async () => {
               <thead>
                 <tr>
                   <th scope="col">ID</th>
-                  <th scope="col">NOMBRE</th>
+                  <th scope="col">USUARIO</th>
                   <th scope="col">CONTRASEÑA</th>
                   <th scope="col">ROL</th>
                   <th scope="col">ACCIONES</th>

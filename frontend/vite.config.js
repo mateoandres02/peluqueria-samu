a9554c58d44e303b70d1b2eaaf88299d5e937375
Mateo Andres <111398187+mateoandres02@users.noreply.github.com>
2024-08-14 23:53:15 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,32 +1,20 @@
-// import { defineConfig } from 'vite';
-// import { createHtmlPlugin } from 'vite-plugin-html';
-
-// export default defineConfig({
-//   build: {
-//     outDir: 'dist', // Directorio de salida
-//     rollupOptions: {
-//       input: {
-//         main: 'index.html',
-//         login: 'login.html', // Añade cualquier otro archivo HTML aquí
-//       },
-//     },
-//   },
-//   plugins: [
-//     createHtmlPlugin({
-//       minify: true,
-//     }),
-//   ]
-// });
-
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
   build: {
+    outDir: 'dist', // Directorio de salida
     rollupOptions: {
       input: {
-        main: './index.html',
-        login: './login.html'
-      }
-    }
-  }
-})
+        main: 'index.html',
+        login: 'login.html', // Añade cualquier otro archivo HTML aquí
+      },
+    },
+  },
+  plugins: [
+    createHtmlPlugin({
+      minify: true,
+    }),
+  ]
+});
+

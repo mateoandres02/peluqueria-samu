686cce007a9c286dbfcf7528fe7e844df7c91982
Facundo Andrean <facundoandrean22@gmail.com>
2024-12-24 17:17:20 -0300

    problema sesion celular

--- backend/app/middlewares/cors.js
+++ backend/app/middlewares/cors.js
@@ -1,35 +1,35 @@
-// const allowedOrigins = ['https://peluqueria-invasion.vercel.app', 'http://localhost:5173'];
-
-// export const corsOptions = {
-//   origin: function (origin, callback) {
-//     // Verificamos si el origen de la petición que llega está en la lista de orígenes permitidos
-//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true, // Permitir el envío de cookies y credenciales
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
-//   allowedHeaders: 'Content-Type,Authorization', // Encabezados permitidos
-// };
-
-const allowedOrigins = [
-  'https://peluqueria-invasion.vercel.app', 
-  'http://localhost:5173'
-];
+const allowedOrigins = ['https://peluqueria-invasion.vercel.app', 'http://localhost:5173'];
 
 export const corsOptions = {
   origin: function (origin, callback) {
-    // Permitir solicitudes sin origen (como en herramientas de desarrollo)
+    // Verificamos si el origen de la petición que llega está en la lista de orígenes permitidos
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true, // Permitir cookies y credenciales
+  credentials: true, // Permitir el envío de cookies y credenciales
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
-  exposedHeaders: ['Set-Cookie'], // Permitir que el cliente acceda a las cookies
+  allowedHeaders: 'Content-Type,Authorization', // Encabezados permitidos
 };
+
+// const allowedOrigins = [
+//   'https://peluqueria-invasion.vercel.app', 
+//   'http://localhost:5173'
+// ];
+
+// export const corsOptions = {
+//   origin: function (origin, callback) {
+//     // Permitir solicitudes sin origen (como en herramientas de desarrollo)
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true, // Permitir cookies y credenciales
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
+//   allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+//   exposedHeaders: ['Set-Cookie'], // Permitir que el cliente acceda a las cookies
+// };

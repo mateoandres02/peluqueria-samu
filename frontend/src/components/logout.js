c81d3778839d9fa69d7a6271c15f4fda324b256e
Facundo Andrean <76758462+facuandrean@users.noreply.github.com>
2025-01-02 21:08:02 -0300

    Update logout.js

--- frontend/src/components/logout.js
+++ frontend/src/components/logout.js
@@ -6,7 +6,8 @@ export const logout = async () => {
     
         if (response.ok) {
             // Eliminar el token de localStorage
-            localStorage.removeItem('accessToken');
+            localStorage.removeItem('token');
+            document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
 
             history.replaceState(null, '', '/login.html');
             history.pushState(null, '', '/login.html');
